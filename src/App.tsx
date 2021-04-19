8f99014e78bc6848078d4fdd0ae94baa583be245
Huynh Duc Dung <dung@aircarbon.co>
2021-04-19 23:27:19 +0800

    refactor: migrate vite v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import AnimeQuotes from "./AnimeQuotes";
-import "./App.css";
 
 const queryClient = new QueryClient();
 
